96810cd8bfc12935b4328c45495673ed4f55cacc
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2023-08-16 15:22:36 +0530

    frontend done

--- pswd-manager/src/App.jsx
+++ pswd-manager/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import GenerateRandomPswd from '../Components/GenerateRandomPswd'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from '../Pages/Signup'
+import Login from '../Pages/Login'
+import Home from '../Pages/Home'
 
 const App = () => {
   return (
    <div className=''>
-    <GenerateRandomPswd/>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/" element={<Home/>} />
+            <Route path="/signup" element={<Signup/>} />
+        </Routes>
+      </BrowserRouter>
    </div>
   )
 }
